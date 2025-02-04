cc41c6af1bd067e81b03e73201daa45e6b2d1b19
Argatu Ioan <154736200+Klipperboi@users.noreply.github.com>
2025-02-04 20:24:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,133 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const particlesJSBackground = document.getElementById('particles-js');
     const sidebar = document.getElementById('sidebar');
     const modeToggle = document.getElementById('modeToggle');
-    const menuToggle = document.createElement('button');
     const img = document.querySelector('img');
     const popup = document.getElementById('imagePopup');
     const popupImage = document.getElementById('popupImage');
     const closePopup = document.getElementById('closePopup');
-    let isSidebarOpen = false;
 
-    // Definește procentul de mărire a imaginii
-    let zoomPercentage = '200%'; // Poți modifica acest procentaj după preferințe
-
-    // Creare buton pentru deschiderea meniului
+    // Butonul pentru meniul lateral
+    const menuToggle = document.createElement('button');
     menuToggle.innerHTML = 'Meniu';
     menuToggle.style.position = 'fixed';
     menuToggle.style.top = '20px';
     menuToggle.style.right = '20px';
     menuToggle.style.zIndex = '1000';
     document.body.appendChild(menuToggle);
+    let isSidebarOpen = false;
 
-    // Deschide/închide meniul
+    function loadParticles(mode) {
+        particlesJSBackground.innerHTML = "";
+        const lineColor = (mode === 'dark') ? '#ffffff' : '#555';
+        const config = {
+            particles: {
+                number: {
+                    value: 80,
+                    density: {
+                        enable: true,
+                        value_area: 800
+                    }
+                },
+                color: {
+                    value: '#ed143d'
+                },
+                shape: {
+                    type: 'circle',
+                    stroke: {
+                        width: 0,
+                        color: '#000000'
+                    }
+                },
+                opacity: {
+                    value: 0.5,
+                    anim: {
+                        enable: false
+                    }
+                },
+                size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                        enable: false
+                    }
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 150,
+                    color: lineColor,
+                    opacity: 0.4,
+                    width: 1
+                },
+                move: {
+                    enable: true,
+                    speed: 2,
+                    direction: 'none',
+                    random: false,
+                    straight: false,
+                    out_mode: 'out',
+                    bounce: false,
+                    attract: {
+                        enable: false
+                    }
+                }
+            },
+            interactivity: {
+                detect_on: 'canvas',
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: 'repulse'
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: 'push'
+                    }
+                },
+                modes: {
+                    grab: {
+                        distance: 200,
+                        line_linked: {
+                            opacity: 1
+                        }
+                    },
+                    bubble: {
+                        distance: 200,
+                        size: 40,
+                        duration: 2,
+                        opacity: 8,
+                        speed: 1
+                    },
+                    repulse: {
+                        distance: 50,
+                        duration: 0.4
+                    },
+                    push: {
+                        particles_nb: 4
+                    },
+                    remove: {
+                        particles_nb: 2
+                    }
+                }
+            },
+            retina_detect: true
+        };
+
+        particlesJS('particles-js', config);
+    }
+
+    // Inițial, se încarcă particulele în modul light
+    loadParticles('light');
+    particlesJSBackground.style.backgroundColor = '#f4f4f4';
+
+    // Eveniment pentru toggle-ul meniului
     menuToggle.addEventListener('click', function() {
         isSidebarOpen = !isSidebarOpen;
         sidebar.style.width = isSidebarOpen ? '250px' : '0';
         sidebar.style.paddingTop = isSidebarOpen ? '20px' : '0';
     });
 
-    // Închide meniul când se apasă în afara lui
+    // Închide meniul când se face click în afara lui
     document.addEventListener('click', function(event) {
         if (isSidebarOpen && !sidebar.contains(event.target) && event.target !== menuToggle) {
             sidebar.style.width = '0';
@@ -35,33 +136,48 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Pop-up pentru imagine
+    // Eveniment pentru deschiderea popup-ului imaginii
     img.addEventListener('click', function() {
         popupImage.src = this.src;
-        popupImage.style.maxWidth = zoomPercentage; // Setează lățimea maximă la valoarea definită
-        popupImage.style.maxHeight = '100vh'; // Setează înălțimea maximă la 100% din înălțimea vizuală a ferestrei
+        popupImage.style.maxWidth = '200%';
+        popupImage.style.maxHeight = '100vh';
         popup.style.display = 'flex';
+        // Dezactivează scroll-ul pe pagină
+        document.body.style.overflow = 'hidden';
     });
 
+    // Eveniment pentru închiderea popup-ului (butonul de închidere)
     closePopup.addEventListener('click', function() {
         popup.style.display = 'none';
+        document.body.style.overflow = 'auto';
     });
 
-    // Închide popup-ul când se face click în afara imaginii sau se apasă Escape
-    popup.addEventListener('click', function(event) {
-        if (event.target === popup) {
-            popup.style.display = 'none';
-        }
-    });
-
+    // Închiderea popup-ului la apăsarea tastei Escape
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Escape') {
             popup.style.display = 'none';
+            document.body.style.overflow = 'auto';
         }
     });
 
-    // Funcția de schimbare a modului întunecat/luminos
+    // Închiderea popup-ului la click pe zona din afara imaginii
+    popup.addEventListener('click', function(event) {
+        // Dacă elementul clicat este popup-ul (și nu imaginea din el), închide popup-ul
+        if (event.target === popup) {
+            popup.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
+
+    // Eveniment pentru schimbarea modului întunecat/luminos
     modeToggle.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            particlesJSBackground.style.backgroundColor = '#333';
+            loadParticles('dark');
+        } else {
+            particlesJSBackground.style.backgroundColor = '#f4f4f4';
+            loadParticles('light');
+        }
     });
 });
